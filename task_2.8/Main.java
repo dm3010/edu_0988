4d6e26e0d9d153e90bd08938efa039b05a5b60c7
dm3010 <88131985+dm3010@users.noreply.github.com>
2021-07-29 17:02:30 +0300

    Update Main.java

--- task_2.8/Main.java
+++ task_2.8/Main.java
@@ -4,7 +4,13 @@
 
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
 
+        int[] a = {2, 3, 4, 5};
+
+        int result = a[0];
+        for (int i = 1; i < a.length; i++) {
+            result *= a[i];
+        }
+        System.out.println(result);
     }
 }
