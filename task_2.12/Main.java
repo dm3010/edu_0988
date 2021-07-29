2a480f630d32cd77cdfae05d3e4e093065a009ef
dm3010 <88131985+dm3010@users.noreply.github.com>
2021-07-29 18:10:37 +0300

    Update Main.java

--- task_2.12/Main.java
+++ task_2.12/Main.java
@@ -3,14 +3,33 @@
 Задание: 1. Создай массив чисел.
 2. Добавь в массив 10 чисел с клавиатуры.
 3. Вывести на экран длину самой длинной последовательности повторяющихся чисел в списке.
-
 Пример для списка 2, 4, 4, 4, 8, 8, 4, 12, 12, 14:
 3
 */
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
 
+        int[] a = new int[10];
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Input array: ");
+        for (int i = 0; i < a.length; i++) {
+            a[i] = scanner.nextInt();
+        }
+
+        int count = 1;
+        int maxCount = 1;
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i] != a[i + 1]) {
+                maxCount = Math.max(maxCount, count);
+                count = 0;
+            }
+            count++;
+        }
+        maxCount = Math.max(maxCount, count);
+        System.out.println(maxCount);
     }
 }
