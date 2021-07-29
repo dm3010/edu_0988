78b7338988fc480e211eb2eff4ac71437c814743
dm3010 <88131985+dm3010@users.noreply.github.com>
2021-07-29 15:41:40 +0300

    Update Main.java

--- task_2.3/Main.java
+++ task_2.3/Main.java
@@ -15,7 +15,15 @@
 
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
+
+        int size = 10;
+
+        for (int i = 1; i <= size; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(8);
+            }
+            System.out.println();
+        }
 
     }
 }
