88aa52453e0b77e71f80b5d82d44dc6d25bcdaca
dm3010 <88131985+dm3010@users.noreply.github.com>
2021-07-29 10:38:41 +0300

    Create Main.java

---
+++ task_1.6/Main.java
@@ -0,0 +1,14 @@
+/*
+Пользователь вводит число, необходимо увеличить это число на 15% и вывести результат на экран
+*/
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        double a = scanner.nextDouble();
+        System.out.println("Результат: " + (a * 115 / 100));
+    }
+}
