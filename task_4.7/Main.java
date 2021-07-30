cb7787202127c4b00bee89254a0f6b75b65851f6
dm3010 <88131985+dm3010@users.noreply.github.com>
2021-07-30 15:04:33 +0300

    Update Main.java

--- task_4.7/Main.java
+++ task_4.7/Main.java
@@ -1,12 +1,13 @@
 /*
 Реализуй в классе Fox интерфейс Animal.
 Учти, что создавать дополнительные классы и удалять методы нельзя!
-
 Требования:
 •	Интерфейс Animal должен быть реализован в классе Fox.
 •	В интерфейсе Animal нужно объявить метод getColor.
 •	Дополнительные классы или интерфейсы создавать нельзя.
 */
+
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args){
@@ -16,8 +17,8 @@ public class Main {
 interface Animal {
     Color getColor();
 }
-    
-class Fox {
+
+abstract class Fox implements Animal {
     public String getName() {
         return "Fox";
     }
