b563fc3bfaff4f2e547c4f1e729156b38eaf6afd
dm3010 <88131985+dm3010@users.noreply.github.com>
2021-07-30 11:52:08 +0300

    Update Main.java

--- task_3.1/Main.java
+++ task_3.1/Main.java
@@ -10,7 +10,10 @@
 
 public class Main {
     public static int min(int a, int b, int c) {
-        //напишите тут ваш код
+        int min = a;
+        if (min > b) min = b;
+        if (min > c) min = c;
+        return min;
     }
 
     public static void main(String[] args) {
