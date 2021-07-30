b520ca1ce72b4a0b4043418ceececa0897f600e4
dm3010 <88131985+dm3010@users.noreply.github.com>
2021-07-30 12:02:52 +0300

    Update Main.java

--- task_3.4/Main.java
+++ task_3.4/Main.java
@@ -1,12 +1,14 @@
 /*
 Задание: Сделайте функцию, которая параметрами принимает 2 числа. Если их сумма больше 10 - пусть функция вернет true, а если нет - false.
-
 */
 
 
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        System.out.println(sumAbove10(3,7));
+    }
 
+    public static boolean sumAbove10(int a, int b) {
+        return a + b > 10;
     }
 }
