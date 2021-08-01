e5e48804061e7f85486b7015508c51abb6349925
dm3010 <88131985+dm3010@users.noreply.github.com>
2021-08-01 15:17:37 +0300

    Update Main.java

--- task_5.1/Main.java
+++ task_5.1/Main.java
@@ -1,11 +1,13 @@
 /*
-* Дана коллекция имён.
-* 1) удалить все повторяющиеся имена из коллекции
-* 2) вывести коллекцию на экран
-* */
+ * Дана коллекция имён.
+ * 1) удалить все повторяющиеся имена из коллекции
+ * 2) вывести коллекцию на экран
+ * */
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,5 +19,12 @@ public class Main {
         names.add("Георгий");
         names.add("Павел");
         names.add("Руслан");
+
+        Set<String> set = new LinkedHashSet(names);
+        names = new ArrayList<>(set);
+
+        for(String name : names)
+            System.out.println(name);
+
     }
 }
