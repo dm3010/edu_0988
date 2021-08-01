35da24f50cd87ddc4ebb4aa2ca55973457e55ffe
dm3010 <88131985+dm3010@users.noreply.github.com>
2021-08-01 15:58:11 +0300

    Update Main.java

--- task_5.3/Main.java
+++ task_5.3/Main.java
@@ -1,11 +1,20 @@
 /*
-* Задание: Дана коллекция с числами. Запишите в новую коллекцию только те числа, которые больше нуля и меньше 10-ти.
-* Коллекции вы создаёте сами
-*/
+ * Задание: Дана коллекция с числами. Запишите в новую коллекцию только те числа, которые больше нуля и меньше 10-ти.
+ * Коллекции вы создаёте сами
+ */
 
-import java.util.ArrayList;
+import java.util.*;
+import static java.util.stream.Collectors.toList;
+
 public class Main {
     public static void main(String[] args) {
-    
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите числа через пробел: ");
+        String[] inputs = scanner.nextLine().split(" +");
+
+        List<Integer> col1 = Arrays.stream(inputs).map(Integer::parseInt).collect(toList());
+        List<Integer> col2 = col1.stream().filter(i -> i > 0 && i < 10).collect(toList());
+
+        System.out.println(col2);
     }
 }
