2655e77d5079a34fcc45edd752bf69ad9e54ad43
dm3010 <dm3010@yandex.ru>
2021-08-05 11:12:19 +0300

    Update Main.java

--- task_4.6/Main.java
+++ task_4.6/Main.java
@@ -9,57 +9,86 @@
 
 public class Main {
     public static void main(String[] args) {
-        Person valera=new Person("Валера","Иванов",69,null,null);
-        Person olga = new Person("Ольга","Иванова",67,null,null);
-        Person oleg = new Person("Олег","Петров",65,null,null);
-        Person jula = new Person("Юля","Петрова",67,null,null);
-        Person alex = new Person("Алексей","Иванов",37,olga,valera);
-        Person eva  = new Person("Ева","Иванова",37,jula,oleg);
-        Person gosha= new Person("Гоша","Иванов",10,eva,alex);
-        System.out.println("Мама Алексея: "+alex.getMother().getName());
-        System.out.println("Бабушка Гоши: "+gosha.getMother().getMother().getName());
+        Person valera = new Person("Валера", "Иванов", 69, null, null);
+        Person olga = new Person("Ольга", "Иванова", 67, null, null);
+        Person oleg = new Person("Олег", "Петров", 65, null, null);
+        Person jula = new Person("Юля", "Петрова", 67, null, null);
+        Person alex = new Person("Алексей", "Иванов", 37, olga, valera);
+        Person eva = new Person("Ева", "Иванова", 37, jula, oleg);
+        Person gosha = new Person("Гоша", "Иванов", 10, eva, alex);
+
+        System.out.println(gosha.genealogic(3,"="));
         System.out.println();
-        System.out.println("Меня зовут " + gosha.getName());
-        System.out.println("Имя моей мамы " + gosha.getMother().getName());
-        System.out.println("Имя моего папы " + gosha.getFather().getName());
-        System.out.printf("У меня две бабушки, бабушка %s и %s%n",
-                gosha.getFather().getMother().getName(),
-                gosha.getMother().getMother().getName());
-        System.out.printf("У меня два деда, деда %s и %s%n",
-                gosha.getFather().getFather().getName(),
-                gosha.getMother().getFather().getName());
+        System.out.println(eva.genealogic(3,"="));
+
+
+//        System.out.println(sd(5));
+        //        System.out.println("Мама Алексея: " + alex.getMother().getName());
+//        System.out.println("Бабушка Гоши: " + gosha.getMother().getMother().getName());
+//        System.out.println();
+//        System.out.println("Меня зовут " + gosha.getName());
+//        System.out.println("Имя моей мамы " + gosha.getMother().getName());
+//        System.out.println("Имя моего папы " + gosha.getFather().getName());
+//        System.out.printf("У меня две бабушки, бабушка %s и %s%n",
+//                gosha.getFather().getMother().getName(),
+//                gosha.getMother().getMother().getName());
+//        System.out.printf("У меня два деда, деда %s и %s%n",
+//                gosha.getFather().getFather().getName(),
+//                gosha.getMother().getFather().getName());
     }
+
+    static String sd(int d) {
+        String f = "";
+        if(d>0)
+            f += sd(d-1);
+        return f+"+";
+    }
+
+
 }
 
-class Person{
+class Person {
     private String name;
     private String lastname;
     private int age;
-    private int hp = 100;
     private Person mother;
     private Person father;
 
-    public Person(String name, String lastname, int age, Person mother, Person father){
+    public Person(String name, String lastname, int age, Person mother, Person father) {
         this.name = name;
         this.lastname = lastname;
         this.age = age;
         this.mother = mother;
         this.father = father;
     }
+
+    public String genealogic(int v, String s) {
+        String info = "";
+        info += s + name + " " + lastname + ", " + age ;
+
+        if(v>1) {
+            if(father!=null) {
+                info += "\n" + father.genealogic(v-1, s+s.charAt(0));
+            }
+            if(mother!=null) {
+                info += "\n" + mother.genealogic(v-1, s+s.charAt(0));
+            }
+            if(father == null && mother == null)
+                info += ": сирота";
+        }
+        return info;
+    }
+
+    public String getLastname() { return lastname; }
+
     public String getName() {
-        return this.name;
+        return name;
     }
-    public int getHp(){
-        return this.hp;
-    }
-    public Person getMother(){
-        return this.mother;
-    }
+
+    public Person getMother() { return mother; }
+
     public Person getFather() {
         return father;
     }
-    public void setHp(int hp){
-        if (this.hp+hp>100) this.hp = 100;
-        else this.hp = this.hp + hp;
-    }
+
 }
